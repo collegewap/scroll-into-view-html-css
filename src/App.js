43a228698a5f1848697d1635ca823c4ceeafb2fc
collegewap <collegewap@gmail.com>
2021-11-10 21:58:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#products">Products</a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          <li>
+            <a href="#about-us">About Us</a>
+          </li>
+        </ul>
+      </header>
+      <div className="section" id="home">
+        <h2>Home</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum, unde!
+          Nisi officia, placeat, enim quibusdam nostrum similique atque
+          accusantium natus sit molestias minima voluptates eos doloribus illum
+          ullam, pariatur fugiat? Minima minus aspernatur, quos, explicabo sed
+          asperiores, enim officia qui voluptates magnam vero aliquid corrupti?
+          Aliquid, est! Expedita tempore impedit fuga eligendi veritatis
+          molestiae ipsa nulla! Est aspernatur eius corrupti.
+        </p>
+      </div>
+      <div className="section" id="products">
+        <h2>Products</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum, unde!
+          Nisi officia, placeat, enim quibusdam nostrum similique atque
+          accusantium natus sit molestias minima voluptates eos doloribus illum
+          ullam, pariatur fugiat? Minima minus aspernatur, quos, explicabo sed
+          asperiores, enim officia qui voluptates magnam vero aliquid corrupti?
+          Aliquid, est! Expedita tempore impedit fuga eligendi veritatis
+          molestiae ipsa nulla! Est aspernatur eius corrupti.
+        </p>
+      </div>
+      <div className="section" id="services">
+        <h2>Services</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum, unde!
+          Nisi officia, placeat, enim quibusdam nostrum similique atque
+          accusantium natus sit molestias minima voluptates eos doloribus illum
+          ullam, pariatur fugiat? Minima minus aspernatur, quos, explicabo sed
+          asperiores, enim officia qui voluptates magnam vero aliquid corrupti?
+          Aliquid, est! Expedita tempore impedit fuga eligendi veritatis
+          molestiae ipsa nulla! Est aspernatur eius corrupti.
+        </p>
+      </div>
+      <div className="section" id="about-us">
+        <h2>About Us</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum, unde!
+          Nisi officia, placeat, enim quibusdam nostrum similique atque
+          accusantium natus sit molestias minima voluptates eos doloribus illum
+          ullam, pariatur fugiat? Minima minus aspernatur, quos, explicabo sed
+          asperiores, enim officia qui voluptates magnam vero aliquid corrupti?
+          Aliquid, est! Expedita tempore impedit fuga eligendi veritatis
+          molestiae ipsa nulla! Est aspernatur eius corrupti.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
